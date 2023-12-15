b8673d938b1772cfbc71fcaf254533f71e1bd79c
victoryforphil <victoryforphil@users.noreply.github.com>
2023-12-15 08:37:53 +0000

    deploy: 012d285ccd142585e55948a233664ce1bc0b754a

---
+++ lil_hopps/control/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"struct":["AngleControl","HeightControl","PIDController","PositionControl","RateControl","UAVController"]};
--- lil_hopps/sidebar-items.js
+++ lil_hopps/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"mod":["simulation","types","uav","viz"]};
+window.SIDEBAR_ITEMS = {"mod":["control","simulation","types","uav","viz"]};
