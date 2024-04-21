30ff78ad59f84725ba9322fe8e6a34897764ff50
Alex Carter <victoryforphil@gmail.com>
2024-04-20 20:49:23 -0700

    [DataBrokerRewrite] Basic UAV and Task Framework. (#24)
    * Base HW/SW defintiions
    
    * Ported Task Manager
    
    * Basic Test Tasks

---
+++ lil-quad/src/uav/tasks/manager.rs
@@ -0,0 +1,297 @@
+use std::{
+    collections::BTreeMap,
+    sync::{Arc, Mutex},
+};
+
+use lil_broker::{DataPoint, Database, QueryCommand, Timestamp, WriteQuery};
+use tracing::{debug, error, info, warn};
+
+
+
+use crate::uav::TaskSubscription;
+
+use super::{Task, TaskMetadata};
+#[derive(Debug, Clone, PartialEq)]
+pub enum TaskStatus {
+    Waiting,
+    Skipping,
+    Running,
+}
+
+pub struct TaskState {
+    pub name: String,
+    pub last_run: Timestamp,
+    pub status: TaskStatus,
+}
+
+impl TaskState {
+    pub fn new(name: String) -> TaskState {
+        TaskState {
+            name,
+            ..Default::default()
+        }
+    }
+}
+
+impl Default for TaskState {
+    fn default() -> Self {
+        TaskState {
+            name: "Default Task".to_string(),
+            last_run: Timestamp::from_seconds(0.0),
+            status: TaskStatus::Waiting,
+        }
+    }
+}
+
+pub type TaskHandle = Arc<Mutex<dyn Task>>;
+
+pub struct TaskEntry {
+    pub name: String,
+    pub task: TaskHandle,
+    pub metadata: TaskMetadata,
+    pub state: TaskState,
+}
+
+impl TaskEntry {
+    pub fn new(task: TaskHandle) -> Self {
+        let task_lock = task.lock().unwrap();
+        let metadata = task_lock.metadata();
+        TaskEntry {
+            name: metadata.name.clone(),
+            task: task.clone(),
+            metadata: metadata.clone(),
+            state: TaskState::new(metadata.name.clone()),
+        }
+    }
+}
+pub struct TaskManager {
+    pub data: Database,
+    pub tasks: Vec<TaskEntry>, // State tracking
+}
+
+impl TaskManager {
+    pub fn new() -> Self {
+        TaskManager {
+            tasks: Vec::new(),
+            data: Database::new(),
+        }
+    }
+    pub fn add_task(&mut self, task: TaskHandle) {
+        let task = TaskEntry::new(task);
+        info!("Adding task: {:?}", task.metadata.name);
+        // Print subscriptions
+        for sub in &task.metadata.subscriptions {
+            info!("  Sub: {:?}", sub);
+        }
+        self.tasks.push(task);
+    }
+
+    pub fn get_task_entry(&self, name: &str) -> Option<&TaskEntry> {
+        self.tasks.iter().find(|t| t.name == name)
+    }
+    // Get the inner task Box
+    pub fn get_task(&self, name: &str) -> Option<&TaskHandle> {
+        self.get_task_entry(name).map(|t| &t.task)
+    }
+
+    // Get the innter task box as mut  ref to the Task
+    pub fn get_task_mut(&mut self, name: &str) -> Option<&mut TaskHandle> {
+        self.tasks
+            .iter_mut()
+            .find(|t| t.name == name)
+            .map(|t| &mut t.task)
+    }
+
+    pub fn tick(&mut self, timestamp: &Timestamp) -> Result<(), anyhow::Error> {
+        info!("TaskManager tick: {:?}", timestamp);
+        for task in self.tasks.iter_mut() {
+            // Get the metadat for the task
+
+            let metadata = &task.metadata;
+            let state = &mut task.state;
+            if timestamp.tick_ms > 0 {
+                if timestamp.tick_ms - state.last_run.tick_ms < metadata.refresh_rate.tick_ms {
+                    state.status = TaskStatus::Skipping;
+                    continue;
+                }
+            }
+            debug!("Running task: {}", metadata.name);
+
+            state.last_run = timestamp.clone();
+            state.status = TaskStatus::Running;
+
+            let subscriptions = &metadata.subscriptions;
+            let query = TaskSubscription::generate_latest_query(subscriptions);
+
+            let data = self.data.query_get_latest(query);
+
+            let data = match data {
+                Ok(data) => data,
+                Err(err) => {
+                    error!("Failed to get data for task: {} {:?}", metadata.name, err);
+                    continue;
+                }
+            };
+            // Select last
+            let inputs: BTreeMap<String, DataPoint> = data
+                .data
+                .into_iter()
+                .map(|(k, v)| {
+                    let last = v.last().unwrap();
+                    (k, last.clone())
+                })
+                .collect();
+
+            let mut task_lock: std::sync::MutexGuard<'_, dyn Task> = task.task.lock().unwrap();
+            let result = task_lock.run(timestamp, &inputs);
+
+            match result {
+                Ok(result) => {
+                    let write_data = result.data;
+                    let mut write_queries: Vec<QueryCommand> = vec![];
+
+                    for (topic, data) in write_data {
+                        let query = WriteQuery::new(topic, data.data, timestamp.clone());
+                        write_queries.push(query.into());
+                    }
+
+                    debug!(
+                        "Task {} completed, wrote {} data points",
+                        metadata.name,
+                        write_queries.len()
+                    );
+                    let _write_result = self.data.query_batch(write_queries);
+                }
+                Err(err) => {
+                    error!("Task {} failed: {:?} ", metadata.name, err);
+                }
+            }
+        }
+
+        debug!("Task Database after tick: {:#?}", self.data.buckets.keys());
+
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use lil_broker::{Primatives, WriteQuery};
+
+  
+    use crate::uav::TaskResult;
+
+    use super::*;
+
+    pub struct TaskA {}
+    pub struct TaskB {}
+
+    impl Task for TaskA {
+        fn metadata(&self) -> TaskMetadata {
+            TaskMetadata::new("task_a".to_string())
+                .with_refresh_rate(Timestamp::from_hz(1.0))
+                .with_subscription("a/input".into())
+        }
+
+        fn run(
+            &mut self,
+            t: &Timestamp,
+            inputs: &BTreeMap<String, DataPoint>,
+        ) -> Result<TaskResult, anyhow::Error> {
+            let mut result = TaskResult {
+                data: BTreeMap::new(),
+                execution_time: Timestamp::zero(),
+            };
+            let topic0 = inputs.get("a/input").unwrap();
+            let topic0_data = topic0.data.clone();
+            let topic0_value = match topic0_data {
+                lil_broker::Primatives::Number(n) => n,
+                _ => return Err(anyhow::anyhow!("Expected Number, got {:?}", topic0_data)),
+            };
+
+            let new_value = topic0_value * 2.0;
+            let new_dp = DataPoint::new(t.clone(), Primatives::Number(new_value));
+
+            result.data.insert("a/output".into(), new_dp);
+            Ok(result)
+        }
+    }
+
+    impl Task for TaskB {
+        fn metadata(&self) -> TaskMetadata {
+            TaskMetadata::new("task_b".to_string())
+                .with_refresh_rate(Timestamp::from_hz(1.0))
+                .with_subscription("b/input".into())
+        }
+
+        fn run(
+            &mut self,
+            t: &Timestamp,
+            inputs: &BTreeMap<String, DataPoint>,
+        ) -> Result<TaskResult, anyhow::Error> {
+            let mut result = TaskResult {
+                data: BTreeMap::new(),
+                execution_time: Timestamp::zero(),
+            };
+            let topic0 = inputs.get("b/input").unwrap();
+            let topic0_data = topic0.data.clone();
+            let topic0_value = match topic0_data {
+                lil_broker::Primatives::Number(n) => n,
+                _ => return Err(anyhow::anyhow!("Expected Number, got {:?}", topic0_data)),
+            };
+
+            let new_value = topic0_value * 3.0;
+            let new_dp = DataPoint::new(t.clone(), Primatives::Number(new_value));
+
+            result.data.insert("b/output".into(), new_dp);
+            Ok(result)
+        }
+    }
+
+    #[test]
+    fn test_task_manager_data_flow() {
+        let mut task_manager = TaskManager::new();
+        let task_a = Arc::new(Mutex::new(TaskA {}));
+        let task_b = Arc::new(Mutex::new(TaskB {}));
+
+        task_manager.add_task(task_a);
+        task_manager.add_task(task_b);
+
+        let mut db: Database = Database::new();
+        let queries = vec![
+            WriteQuery::new("a/input".into(), Primatives::Number(2.0), Timestamp::new(0)).into(),
+            WriteQuery::new("b/input".into(), Primatives::Number(4.0), Timestamp::new(0)).into(),
+        ];
+        db.query_batch(queries).expect("Failed to write queries");
+
+        task_manager.data = db;
+
+        let timestamp = Timestamp::from_seconds(0.0);
+
+        let max_t = Timestamp::from_seconds(1.0);
+        let mut t = timestamp.clone();
+        while t < max_t {
+            task_manager.tick(&t);
+            t = t + Timestamp::from_seconds(1.0);
+        }
+
+        let result_a = task_manager
+            .data
+            .query_get_latest(vec!["a/output".to_string()].into())
+            .unwrap();
+        
+        let a_out = result_a.data.get("a/output").unwrap();
+        assert_eq!(a_out[0].data, Primatives::Number(4.0));
+
+        let result_b = task_manager
+            .data
+            .query_get_latest(vec!["b/output".to_string()].into())
+            .unwrap();
+
+        let b_out = result_b.data.get("b/output").unwrap();
+
+        assert_eq!(b_out[0].data, Primatives::Number(12.0));
+        
+    }
+
+}
---
+++ lil-quad/src/uav/tasks/mod.rs
@@ -0,0 +1,185 @@
+mod manager;
+mod task_utils;
+pub use manager::*;
+pub use task_utils::*;
+
+use std::{any, collections::BTreeMap};
+
+use lil_broker::{DataPoint, GetLatestQuery, Timestamp};
+#[derive(Debug, Clone, PartialEq)]
+pub struct TaskMetadata {
+    pub name: String,
+    pub subscriptions: Vec<TaskSubscription>,
+    pub refresh_rate: Timestamp,
+}
+#[derive(Debug, Clone, PartialEq)]
+pub struct TaskSubscription{
+    pub name: String,
+    pub ack: bool,
+}
+
+impl TaskSubscription{
+    pub fn new(name: String) -> TaskSubscription{
+        TaskSubscription{
+            name,
+            ack: false,
+        }
+    }
+
+    pub fn with_ack(mut self, ack: bool) -> TaskSubscription{
+        self.ack = ack;
+        self
+    }
+
+    pub fn generate_latest_query(subs: &Vec<TaskSubscription>) -> GetLatestQuery{
+        let topics = subs.iter().map(|sub| sub.name.clone()).collect();
+        let acks = subs.iter().filter(|sub| sub.ack).map(|sub| sub.name.clone()).collect();
+        GetLatestQuery{
+            topics,
+            ack_topics: acks,
+            tag_filters: Vec::new(),
+        }
+    }
+}
+
+impl From<String> for TaskSubscription{
+    fn from(name: String) -> Self{
+        TaskSubscription::new(name)
+    }
+}
+
+impl From<&str> for TaskSubscription{
+    fn from(name: &str) -> Self{
+        TaskSubscription::new(name.into())
+    }
+}
+
+impl TaskMetadata {
+    pub fn new(name: String) -> TaskMetadata {
+        TaskMetadata {
+            name,
+            ..Default::default()
+        }
+    }
+
+    pub fn with_subscription(mut self, subscription: TaskSubscription) -> TaskMetadata {
+        self.subscriptions.push(subscription);
+        self
+    }
+
+    pub fn with_subscriptions(mut self, subscriptions: Vec<TaskSubscription>) -> TaskMetadata {
+        self.subscriptions.extend(subscriptions);
+        self
+    }
+
+    pub fn with_refresh_rate(mut self, refresh_rate: Timestamp) -> TaskMetadata {
+        self.refresh_rate = refresh_rate;
+        self
+    }
+
+    pub fn with_refresh_rate_hz(mut self, hz: f32) -> TaskMetadata {
+        self.refresh_rate = Timestamp::from_hz(hz);
+        self
+    }
+}
+
+impl Default for TaskMetadata {
+    fn default() -> Self {
+        TaskMetadata {
+            name: "Default Task".to_string(),
+            subscriptions: Vec::new(),
+            refresh_rate: Timestamp::from_hz(100.0 as f32), // 100 Hz
+        }
+    }
+}
+
+pub struct TaskResult {
+    pub data: BTreeMap<String, DataPoint>,
+    pub execution_time: Timestamp,
+}
+
+pub trait Task {
+    fn metadata(&self) -> TaskMetadata;
+    fn run(
+        &mut self,
+        t: &Timestamp,
+        inputs: &BTreeMap<String, DataPoint>,
+    ) -> Result<TaskResult, anyhow::Error>;
+}
+
+// --- Mock Section --- //
+
+/// Mock implementation of the Task trait
+/// Inputs:
+/// - inputs: A map of topic names to DataPoints
+///    - `/topic/0`: DataPoint::Number
+/// Outputs:
+/// - data: Output data for writing by the task
+///     - `/debug/0`: [DataPoint::String]
+pub struct MockTask {}
+
+impl Task for MockTask {
+    fn metadata(&self) -> TaskMetadata {
+        TaskMetadata::new("Mock Task".into())
+            .with_refresh_rate(Timestamp::from_hz(100.0 as f32))
+            .with_subscription("/topic/0".into())
+    }
+
+    fn run(
+        &mut self,
+        t: &Timestamp,
+        inputs: &BTreeMap<String, DataPoint>,
+    ) -> Result<TaskResult, anyhow::Error> {
+        let mut result = TaskResult {
+            data: BTreeMap::new(),
+            execution_time: Timestamp::zero(),
+        };
+        let topic0 = inputs.get("/topic/0").unwrap();
+        let topic0_data = topic0.data.clone();
+        let topic0_value = match topic0_data {
+            lil_broker::Primatives::Number(n) => n,
+            _ => return Err(anyhow::anyhow!("Expected Number, got {:?}", topic0_data)),
+        };
+
+        let debug_message = format!("topic_0={}", topic0_value);
+        let debug_message_dp =
+            DataPoint::new(t.clone(), lil_broker::Primatives::String(debug_message));
+        result
+            .data
+            .insert("/debug/0".into(), debug_message_dp);
+        Ok(result)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use pretty_assertions::assert_eq;
+    #[test]
+    fn test_mock_task_metadata() {
+        let metadata = MockTask {}.metadata();
+        assert_eq!(metadata.name, "Mock Task");
+        assert_eq!(metadata.subscriptions.len(), 1);
+        assert_eq!(metadata.subscriptions[0], "/topic/0".into());
+        assert_eq!(metadata.refresh_rate, Timestamp::from_hz(100.0 as f32));
+    }
+
+    #[test]
+    fn test_mock_task_run() {
+        let mut task = MockTask {};
+        let t = Timestamp::new(0);
+        let inputs = {
+            let mut map = BTreeMap::new();
+            map.insert("/topic/0".into(), DataPoint::new(t, 5.0.into()));
+            map
+        };
+        let result = task.run(&t, &inputs).unwrap();
+
+        assert_eq!(result.data.len(), 1);
+        assert_eq!(result.execution_time, Timestamp::zero());
+
+        let debug_messages = result.data.get("/debug/0").unwrap();
+        let debug_message = &debug_messages;
+        assert_eq!(debug_message.data, lil_broker::Primatives::String("topic_0=5".to_string()));
+    }
+}
