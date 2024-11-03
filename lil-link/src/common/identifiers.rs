256d5901fa8dd5027f6b952b219b2e3e4ea30c79
Alex Carter <victoryforphil@gmail.com>
2024-11-03 13:31:55 -0800

    [Link] Added basic battery info

--- lil-link/src/common/identifiers.rs
+++ lil-link/src/common/identifiers.rs
@@ -9,6 +9,12 @@ pub static IDENT_STATUS_TEXT: &str = "text";
 pub static IDENT_STATUS_SENSORS: &str = "sensors";
 pub static IDENT_STATUS_EKF: &str = "ekf";
 pub static IDENT_STATUS_HEALTH: &str = "health";
+pub static IDENT_STATUS_BATTERY: &str = "battery";
+pub static IDENT_STATUS_DROP_RATE_COMM: &str = "drop_rate_comm";
+pub static IDENT_STATUS_VOLTAGE: &str = "voltage";
+pub static IDENT_STATUS_CURRENT: &str = "current";
+pub static IDENT_STATUS_COMM_ERRORS: &str = "comm_errors";
+pub static IDENT_STATUS_ERRORS_COUNT: &str = "errors_count";
 pub static IDENT_COMMAND_ACK: &str = "command_ack";
 
 pub static IDENT_POSE_NED: &str = "ned";
