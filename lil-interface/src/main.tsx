0b895bafb72460202dd56dbda7ccd61a1dc91d10
hices <shawnhice@gmail.com>
2024-11-05 00:17:13 -0800

    Added basic charting -- tested and working

--- lil-interface/src/main.tsx
+++ lil-interface/src/main.tsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './style/index.scss';
 import App from './App.tsx';
+import './style/index.scss';
 import '@mantine/core/styles.css';
 import '@mantine/notifications/styles.css';
+import '@mantine/charts/styles.css';
 import { MantineProvider } from '@mantine/core';
 import { Notifications } from '@mantine/notifications';
 
